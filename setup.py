75aaee3caf84ac2869a2c824d932233745a54f41
Lucas Siemons <lucassiemons@Lucass-MBP.lan>
2023-01-31 11:14:23 +0100

    23/01/2023 some changes to set up - kept cython stuff commented out

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup, find_packages
+#from Cython.Build import cythonize
+import numpy as np
 
 descrip='''
 ====// --------- \\\\====
@@ -13,10 +15,12 @@ setup(
     author='L. Siemons',
     author_email='lucas.siemons@googlemail.com',
     packages=find_packages(),
+    #ext_modules=cythonize("data2ensembles/csa_spec_dens.pyx"),
     #license='LICENSE.txt',
-    package_data={'': ['data2ensembles/dat/*']},
+    package_data={'data2ensembles': ['dat/*dat']},
     include_package_data=True,
     description=descrip,
+    include_dirs=[np.get_include()]
     #long_description=open('README.md').read(),
     #install_requires=['numpy','scipy>=0.17.0', 'matplotlib'],
 )
