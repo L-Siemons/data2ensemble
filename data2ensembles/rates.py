73fe8d4484a47c6191f284b8948e3f3fa8ad9085
Lucas Siemons <lucassiemons@Lucass-MBP.lan>
2023-04-14 09:51:09 +0200

    2023/04/14 added new rate functions
    two spin order: r_XzYz()
    seperated R1 into dipolar and CSA
    added cross correlated relaxation pathway delta_rate

--- data2ensembles/rates.py
+++ data2ensembles/rates.py
@@ -78,23 +78,20 @@ def anisotropic_csa_in_anisotropic_diffusion(params, spectral_density, omega,
     return term1 + term2 + term3
 
 def r1_YX_dipollar(params, spectral_density, fields,
-    rxy, x, y='h', 
-    cosine_angles = [], omega_x=None, omega_y=None,
+    rxy, x, y='h', cosine_angles = [], omega_x=None, omega_y=None,
     PhysQ=PhysicalQuantities):
     '''
     This function calculates only the dipolar contributions to the R1
     '''
 
-    if omega_x.all() == None:
+    if type(omega_x) != np.ndarray:
         omega_x = PhysQ.calc_omega(x, fields)
-    if omega_y.all() == None:
+    if type(omega_y) != np.ndarray:
         omega_y = PhysQ.calc_omega(y, fields)  
 
-    print('here boiii', omega_y)
-
     dd_prefactor = (PhysQ.calc_dd(x,y,rxy)**2)/4
-    omega_x = PhysQ.calc_omega(x, fields)
-    omega_y = PhysQ.calc_omega(y, fields)  
+    #omega_x = PhysQ.calc_omega(x, fields)
+    #omega_y = PhysQ.calc_omega(y, fields)  
 
     term1 = dd_prefactor*(spectral_density(params, [omega_y-omega_x]+cosine_angles) + \
                        3.*spectral_density(params, [omega_x]+cosine_angles) + \
@@ -102,18 +99,76 @@ def r1_YX_dipollar(params, spectral_density, fields,
 
     return term1
 
+def r_XzYz(params, spectral_density, fields,
+    rxy, csa_atom_name, x, y='h', 
+    cosine_angles = [], csa_cosine_angles=None, csa_params=None,
+    PhysQ=PhysicalQuantities, model='anisotropic'):
+    '''
+    This function calculates only the dipolar contributions to the R(IzSz) relaxation rate
+    This function does not include the contributions from the spin latice. These need to be
+    added serperately.
+
+    The form is taken from 
+
+    The Complete Homogeneous Master Equation for a Heteronuclear Two-Spin 
+    System in the Basis of Cartesian Product Operators
+    Peter Allard, Magnus Helgstrand, and Torleif Hard (1997) 
+
+    '''
+
+    omega_x = PhysQ.calc_omega(x, fields)
+    omega_y = PhysQ.calc_omega(y, fields)  
+
+    dd_prefactor = 3*(PhysQ.calc_dd(x,y,rxy)**2)/4
+
+    term1 = 3*dd_prefactor*(spectral_density(params, [omega_x]+cosine_angles) +\
+                    spectral_density(params, [omega_y]+cosine_angles))
+
+    if model == 'anisotropic':
+
+        csa_prefactor = PhysQ.calc_aniso_csa_prefactor(fields, y, csa_atom_name)
+        csa_j_term = anisotropic_csa_in_anisotropic_diffusion(params, spectral_density, omega_y,
+        csa_atom_name, csa_params ,csa_cosine_angles, 
+        PhysQ=PhysQ)
+
+        #check the csa prefactor is right!
+        term2 = csa_j_term
+
+    if model == 'axially symmetric':
+        csa_prefactor = PhysQ.calc_axially_symetric_csa(fields, y, csa_atom_name)**2
+        term2 = spectral_density(params, [omega_y]+csa_cosine_angles)
+
+    term2a = term2*csa_prefactor
+    term3 = term1 + term2a
+    #print(term1[0], term2a[0])
+    return term3
+
+
 def r1_YX_csa(params, spectral_density, fields,
     csa_atom_name, x, y, csa_cosine_angles, csa_params,
-    PhysQ):
+    PhysQ, omega_x=None, model='anisotropic'):
 
-    csa_j_term = anisotropic_csa_in_anisotropic_diffusion(params, spectral_density, omega_x,
-    csa_atom_name, csa_params ,csa_cosine_angles, 
-    PhysQ=PhysQ)
+    if omega_x.all() == None:
+        omega_x = PhysQ.calc_omega(x, fields)
 
-    csa_prefactor = PhysQ.calc_aniso_csa_prefactor(fields, x, csa_atom_name)
 
-    #check the csa prefactor is right!
-    term2 = csa_prefactor*csa_j_term
+    
+    if model == 'anisotropic':
+
+        csa_prefactor = PhysQ.calc_aniso_csa_prefactor(fields, x, csa_atom_name)
+        csa_j_term = anisotropic_csa_in_anisotropic_diffusion(params, spectral_density, omega_x,
+        csa_atom_name, csa_params ,csa_cosine_angles, 
+        PhysQ=PhysQ)
+
+        #check the csa prefactor is right!
+        term2 = csa_prefactor*csa_j_term
+        return term2
+
+    if model == 'axially symmetric':
+        csa_prefactor = PhysQ.calc_axially_symetric_csa(fields, x, csa_atom_name)**2
+        term2 = csa_prefactor*spectral_density(params, [omega_x]+csa_cosine_angles)
+        return term2
+
 
     # this line below is for the axially symetric CSA 
     # term2 = csa_prefactor*spectral_density(params, [omega_x]+csa_cosine_angles)
@@ -125,7 +180,7 @@ def r1_YX_csa(params, spectral_density, fields,
 def r1_YX(params, spectral_density, fields,
     rxy, csa_atom_name, x, y='h', 
     cosine_angles = [], csa_cosine_angles=None, csa_params=None,
-    PhysQ=PhysicalQuantities):
+    PhysQ=PhysicalQuantities, model='anisotropic'):
 
     '''
     This function calculates the R1 rate for atom X in a X-Y spin
@@ -144,16 +199,17 @@ def r1_YX(params, spectral_density, fields,
 
     term1 = r1_YX_dipollar(params, 
         spectral_density, 
-        fields, rxy, x, y='h', 
-        cosine_angles = csa_cosine_angles, 
+        fields, rxy, x, y=y, 
+        cosine_angles = cosine_angles, 
         omega_x=omega_x, 
         omega_y=omega_y,
         PhysQ=PhysicalQuantities)
 
     term2 = r1_YX_csa(params, spectral_density, fields,
     csa_atom_name, x, y, csa_cosine_angles, csa_params,
-    PhysQ)
+    PhysQ, omega_x=omega_x, model=model)
 
+    #print(term1, term2)
     return term1 + term2
 
 
@@ -200,9 +256,6 @@ def r2_YX(params, spectral_density, fields,
     PhysQ=PhysQ)
 
     term2 = csa_prefactor*(4*csa_j_term_0 + 3* csa_j_term_omega_x)
-
-    # term2 = csa_prefactor*(4*spectral_density(params,[ 0.]+csa_cosine_angles) \
-    #                      + 3*spectral_density(params, [omega_x]+csa_cosine_angles))
  
     return term1 + term2
 
@@ -260,4 +313,45 @@ def r1_reduced_noe_YX(params, spectral_density, fields,
            (6*spectral_density(params, [omega_x+omega_y]+cosine_angles) \
             - spectral_density(params, [omega_y-omega_x]+cosine_angles))
 
+    return term2
+
+def delta_rate(params, spectral_density, fields,
+    rxy, csa_atom_name, x, y='h', 
+    csa_cosine_angles=None, csa_params=None,
+    PhysQ=PhysicalQuantities, model='anisotropic'):
+
+    '''
+    This rate describes the cross correlated relaxation between Iz and IzSz.
+    The form is taken from 
+
+    The Complete Homogeneous Master Equation for a Heteronuclear Two-Spin 
+    System in the Basis of Cartesian Product Operators
+    Peter Allard, Magnus Helgstrand, and Torleif Hard (1997)
+    '''
+
+    omega_x = PhysQ.calc_omega(x, fields)
+    omega_y = PhysQ.calc_omega(y, fields)
+
+    csa_prefactor = PhysQ.calc_aniso_csa_prefactor(fields, x, csa_atom_name, square=False)
+    dd_prefactor = PhysQ.calc_dd(x,y,rxy)/4
+
+    #anisotropic CSA 
+    if model == 'anisotropic':
+
+        csa_prefactor = PhysQ.calc_aniso_csa_prefactor(fields, x, csa_atom_name, square=False)
+        #print(f"csa {csa_prefactor}")
+        csa_j_term = anisotropic_csa_in_anisotropic_diffusion(params, spectral_density, omega_x,
+        csa_atom_name, csa_params ,csa_cosine_angles, 
+        PhysQ=PhysQ)
+
+    # axially symetric CSA
+    elif model == 'axially symmetric':
+        csa_prefactor = PhysQ.calc_axially_symetric_csa(fields, x, csa_atom_name)
+        #print(f"csa {csa_prefactor}")
+        csa_j_term = spectral_density(params, [omega_x]+csa_cosine_angles)
+    else:
+        print('model for delta_rate not selected')
+
+    #print(csa_prefactor, dd_prefactor, csa_j_term)
+    term2 = csa_prefactor*dd_prefactor*csa_j_term
     return term2
