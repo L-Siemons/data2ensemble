b5576fe49bf93b798e04aaee481f63bde840e04b
Lucas Siemons <lucassiemons@Lucass-MacBook-Pro.local>
2023-07-31 11:03:34 +0200

    2023/07/31 re-wrote the shuttling trajectory
    Here I re-wrote how the shuttling trajectory is calculated.
    Now it is correct and uses an interpolation method to go between the distance
    and the field

--- data2ensembles/shuttling.py
+++ data2ensembles/shuttling.py
@@ -58,7 +58,7 @@ class ShuttleTrajectory():
 
     """
 
-    def __init__(self, fields_file, magnetic_fields_trajectory_file='default', field_increment=0.1):
+    def __init__(self, fields_file, magnetic_fields_trajectory_file='default', field_increment=0.5):
 
         # load the magnetic field file v distance
         if magnetic_fields_trajectory_file == 'default':
@@ -113,7 +113,7 @@ class ShuttleTrajectory():
 
         self.sampled_fields = np.array(self.sampled_fields)
 
-    def construct_single_trajectory(self, field, all_dists, all_fields):
+    def construct_single_trajectory(self, field, points=20):
         '''
         This funcion calculates the time spent at each field during shuttling. 
         Here we assume that shuttling occurs using a linear acceleration to the 
@@ -142,27 +142,52 @@ class ShuttleTrajectory():
             the time spent at each field center. 
         '''
 
-        # get the travel time set by the user 
+        #these are the points we travel through
+        high_field_possition = self.experiment_info[field]['high_field']
+        max_field_in_interpolator = np.max(self.distance_field_measured.T[1])
+
+        fields_trajectory = np.geomspace(max_field_in_interpolator, field, points)
+        distances = self.field_to_distance(fields_trajectory)
+
+        # for debugging!
+        # print(field)
+        # print(max_field_in_interpolator)
+        # print('F', fields_trajectory)
+        # print('Dist', distances)
+        # print(self.fields_distances_dict[field])
+
+        # get the travel time set by the user
         travel_time = self.experiment_info[field]['travel_time']
         half_time = travel_time/2
 
         
         # the distance that corresponds to the field
-        total_distance = self.fields_distances_dict[field]
+        # here I could use the know distance but this might 
+        # cause some slightly funny behaviour if there is a small error in the interpolator
+        total_distance = np.max(distances)
         half_distance = total_distance/2
 
+        # now we add the zero point for the highfield and also the point where we change acceleration
+        midpoint_field = self.distance_to_field(half_distance)
+
+        fields_trajectory = np.concatenate([[high_field_possition, midpoint_field], fields_trajectory])
+        distances = np.concatenate([[0, half_distance], distances])
+        indices = np.argsort(distances)
+        distances = distances[indices]
+        fields_trajectory = fields_trajectory[indices]
+
+
+        # calculate the acceleration
         acceleration = half_distance*2/(half_time**2)
         decceleration = -1*acceleration
         max_velocity = acceleration*half_time
 
-        #the distances where we are accelerating
-        acc_selector = (all_dists<half_distance)
-        acc_distances = all_dists[acc_selector]
+        #the distances where we are accelerating, include the midpoint in the acceleration
+        acc_distances = distances[distances <= half_distance]
         acc_time = np.sqrt(2*acc_distances/acceleration)
 
         #the distances where we are decellerating
-        deccel_selector = (all_dists >= half_distance) &  (all_dists < total_distance)
-        decc_distances = all_dists[deccel_selector] - half_distance
+        decc_distances = distances[distances > half_distance] - half_distance
 
         #to get the decelleration times we will use the numpy solver 
         a = 0.5*decceleration
@@ -173,14 +198,14 @@ class ShuttleTrajectory():
         decc_distances = decc_distances+half_distance
 
         # sort the decellerating times
-        indices = np.argsort(decc_times)
-        decc_times = decc_times[indices]
-        decc_distances = decc_distances[indices]
+        # indices = np.argsort(decc_times)
+        # decc_times = decc_times[indices]
+        # decc_distances = decc_distances[indices]
 
         # sort the accelerating times
-        indices = np.argsort(acc_time)
-        acc_time = acc_time[indices]
-        acc_distances = acc_distances[indices]
+        # indices = np.argsort(acc_time)
+        # acc_time = acc_time[indices]
+        # acc_distances = acc_distances[indices]
 
         # print('decc_distances', decc_distances)
         # print('decc_times', decc_times)
@@ -192,10 +217,12 @@ class ShuttleTrajectory():
         # plt.xlabel('time (s)')
         # plt.ylabel('distance (m)')
         # plt.legend()
+        # plt.savefig(f'{field}_time_v_distance.pdf')
         # plt.show()
 
         total_time = np.concatenate([acc_time, decc_times])
         total_distances =  np.concatenate([acc_distances, decc_distances])
+
         acc_fields = self.distance_to_field(acc_distances)
         decc_fields = self.distance_to_field(decc_distances)
         
@@ -212,20 +239,30 @@ class ShuttleTrajectory():
         # plt.xlabel('time (s)')
         # plt.ylabel('field (T)')
         # plt.legend()
+        # plt.savefig(f'{field}_time_v_field.pdf')
         # plt.show()        
 
         # #time spent at each field going forwards
         field_centers = (total_fields[1:] + total_fields[:-1]) / 2
         time_taken = total_time[1:] - total_time[:-1]
+        # print('sum time', np.sum(time_taken))
+
+
+        # print('field', field)
+        # print('Field centers ', field_centers)
+        # print('total fields', total_fields)
+        # print('time_taken', time_taken)
+        # print('total time', sum(time_taken))
         
         # print('time_taken', time_taken)
         # print('time_taken sum ', sum(time_taken))
 
-        # # #plot time at each field
+        #plot time at each field
         # plt.plot(time_taken, field_centers)
         # plt.scatter(time_taken, field_centers)
         # plt.xlabel('time (s)')
         # plt.ylabel('field (T)')
+        # plt.savefig(f'{field}_DetlaTime_v_field.pdf')
         # plt.show()        
 
         return field_centers, time_taken
@@ -241,25 +278,15 @@ class ShuttleTrajectory():
         # I have used self.fields_distances instead of all_dists and all_fields
         # so I need to re-write this using these instead. 
         
-        # print(all_fields.shape)
-        # print(all_dists.shape)
-
         for field in self.sampled_fields: 
             # print(field)
-            forwards = self.construct_single_trajectory(field, all_dists_master, all_fields_master)
+            forwards = self.construct_single_trajectory(field)
             forwards_field_center, forwards_time_taken = forwards
-
-            reverse_selector = (all_fields_master>=field)
-            all_fields = np.flip(all_fields_master[reverse_selector])
-            all_dists = np.flip(self.fields_distances_dict[field] - all_dists_master[reverse_selector])
 
             # since the path is symetrical we can just do the reverse of the forwards path
             backwards_field_center = np.flip(forwards_field_center)
             backwards_time_taken = np.flip(forwards_time_taken)
 
-            # print('back')
-            # backwards = self.construct_single_trajectory(field, all_dists, all_fields)
-            # backwards_field_center, backwards_time_taken = backwards
             self.trajectory_time_at_fields[field] = [forwards_field_center, forwards_time_taken, 
                                                     backwards_field_center, backwards_time_taken]
 
